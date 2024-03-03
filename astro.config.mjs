bbf4a5e73edf80c350093285c6868df44a11a3d3
yukihiro.enomoto <yukihiro.enomoto@megafoon.co.jp>
2024-03-03 16:27:09 +0900

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,25 @@
 import { defineConfig } from 'astro/config';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    port: 8080
+  }
+  // vite: {
+  //   css: {
+  //     preprocessorOptions: {
+  //       scss: {
+  //         additionalData: `@import "src/scss/style.scss";`
+  //       }
+  //     }
+  //   }
+  // },
+  // experimental: {
+  //   router: 'spa'
+  // }
+  ,
+  output: "server",
+  adapter: vercel()
+});
